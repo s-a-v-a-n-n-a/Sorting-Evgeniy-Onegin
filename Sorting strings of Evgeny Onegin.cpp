5fc6be9d52f868b64baabeeef12f0e1bcd1a8c79
s-a-v-a-n-n-a <70327418+s-a-v-a-n-n-a@users.noreply.github.com>
2020-09-13 12:32:13 +0300

    Update Sorting strings of Evgeny Onegin.cpp

--- Sorting strings of Evgeny Onegin.cpp
+++ Sorting strings of Evgeny Onegin.cpp
@@ -1,21 +1,13 @@
 #include <stdio.h>
-#include <locale.h>
+#include <stdlib.h>
+//#include <locale.h>
 #include <assert.h>
+#include <ctype.h>
 
 typedef long int LONG;
 
 #define MAXSYMB 100
 #define MAXLINES 1000
-
-//-------------------------------------------------------------
-// Copies one string to another
-//
-// Parameters:
-//
-// *line1[]        address of the line 1 of char
-// *line2[]        address of the line 2 of char
-//-------------------------------------------------------------
-void strCopy(char *line1[], char *line2[]);
 
 //-------------------------------------------------------------
 // Compares one string to another
@@ -33,125 +25,262 @@ void strCopy(char *line1[], char *line2[]);
 //-------------------------------------------------------------
 int  compStr(char *line1[], char *line2[]);
 
+void putTheHoleLine(char *line, LONG SiZe);
+
+void putLine(char *ptr[], char *line, LONG nlines);
+
 //-------------------------------------------------------------
-// Prints the string in the file
+// Scans the string from the file
 //
 // Parameters:
 //
 // *line[]        address of the massive of pointers on strings
+// *file          pointer on the input file
+//
+// Returns:
+//
+// nlines         the number of scanned strings
 //-------------------------------------------------------------
-void putLine(char *line[]);
+LONG getLines(char *buf, char *ptr_mas[]);
 
-char getLine(char *line[], LONG num_line, int max_len, FILE *file);
-
+//-------------------------------------------------------------
+// Sorts the strings
+//
+// Parameters:
+//
+// *line[]        address of the massive of pointers on strings
+// low            the first index of the sorting part
+// up             the last index of the sorting part
+//-------------------------------------------------------------
 void sortLines(char *line[], LONG low, LONG up);
 
-void swapLines(char *line1[], LONG i, LONG j);
+//-------------------------------------------------------------
+// Swaps two strings
+//
+// Parameters:
+//
+// *Str[]        address of the massive of pointers on strings
+// i             the index of the first string
+// j             the index of the second string
+//-------------------------------------------------------------
+void swapLines(char *Str[], LONG i, LONG j);
 
+void unit_tests_for_compstr();
 
-//void unit_tests_for_strcopy();
-
-//void unit_tests_for_compstr();
-
+void unit_tests_for_sortlines();
 
 int main(int argc, const char *argv[])
 {
-    setlocale(LC_ALL, "Rus");
+    //setlocale(LC_ALL, "Rus");
     FILE *poem;
 
     if (argc > 0)
     {
-        poem = fopen(argv[1], "r");
+        poem = fopen(argv[1], "rb");
         if (poem == NULL)
             return 0;
     }
 
+    LONG Len = 0;                  //СЃРјРѕС‚СЂСЋ СЂР°Р·РјРµСЂ РјР°СЃСЃРёРІР° Рё СЃРѕР·РґР°СЋ Р±СѓС„РµСЂ
+    if (!fseek(poem, 0, SEEK_END))
+         Len = ftell(poem);
+    Len++;
+    fseek(poem, 0, SEEK_SET);
 
-    char *line[MAXLINES];//массив указателей на char
+    //printf("%d\n", Len);
+
+    char *ptr_buf[MAXLINES] = {};//РјР°СЃСЃРёРІ СѓРєР°Р·Р°С‚РµР»РµР№ РЅР° char
+    char *buffer = (char *) calloc(Len, sizeof(char));
+
+    LONG obj = fread(buffer, sizeof(char), Len, poem);
+
+    //printf("%d\n", obj);
+    //printf("There's no EOF\n%d\nThere's an error\n%d\n", feof(poem), ferror(poem));
+
+    assert(obj == Len - 1);
+
+    buffer[Len - 1] = '\0';
 
     LONG nlines = 0;
 
-    for (LONG kol = 0; ; (*line)++, kol++)//увеличивается указатель на нулевой элемент массива
-        if (getLine(line, kol, MAXSYMB, poem) == EOF)//в функцию передаётся указатель на элемент kol
-            break;
+    nlines = getLines(buffer, ptr_buf);
 
-    sortLines(line, 0, nlines);
+    printf("%d\n", nlines);
 
-    putLine(line);
+    unit_tests_for_compstr();
+    unit_tests_for_sortlines();
+
+    //putLine(buffer, Len);
+
+    sortLines(ptr_buf, 0, nlines - 2);
+
+    putTheHoleLine(buffer, Len);
+
+    printf("Write smth\n");
+
+    putLine(ptr_buf, buffer, nlines - 2);
 
     fclose(poem);
+
+    free(buffer);
 
     return 0;
 }
 
-void strCopy(char line1[], char line2[])//получает указатели на массивы из char
+int compStr(char *line1, char *line2)
 {
-    while(*line1++ = *line2++)
-        ;
-}
+    while(*line1 == *line2 && *line1 != '\n' && *line2 != '\n')
+    {
+        line1++;
+        line2++;
 
-int compStr(char line1[], char line2[])
-{
-    while(*line1++ == *line2++)
-        ;
-    if (*line1 == *line2 == NULL)
-        return 0;
-    else if (*line1 > *line2)
-        return 1;
-    else
+        while (!isalnum(*line1) && *line1 != '\n')
+            line1++;
+        while (!isalnum(*line2) && *line2 != '\n')
+            line2++;
+    }
+    //РІСЂРѕРґРµ РєР°Рє РµСЃР»Рё РѕРґРЅР° СЃС‚СЂРѕС‡РєР° СѓР¶Рµ Р·Р°РєРѕРЅС‡РёР»Р°СЃСЊ, С‚Рѕ '\n'<РІСЃРµС… РѕСЃС‚Р°Р»СЊРЅС‹С… РІРѕР·РјРѕР¶РЅС‹С… СЃРёРјРІРѕР»РѕРІ,
+    //С‚Р°Рє С‡С‚Рѕ РЅРµ РЅСѓР¶РЅРѕ РїСЂРѕРІРµСЂСЏС‚СЊ РѕС‚РґРµР»СЊРЅРѕ
+    if      (isalnum(*line1) && isalnum(*line2) && *line1 > *line2)
+        return  1;
+    else if (isalnum(*line1) && isalnum(*line2) && *line1 < *line2)
         return -1;
+    else return 0;
 }
 
-void sortLines(char *line[], LONG low, LONG up)//функция получает указатель на массив указателей на char
+void unit_tests_for_compstr()
+{
+    assert(compStr("", "") == 0);
+
+    assert(compStr("        ", "") == 0);
+
+    assert(compStr(" ", "  ") == 0);
+
+    assert(compStr(",", " ") == 0);
+
+    assert(compStr("Meow\n", "M   eow\n") == 0);
+
+    assert(compStr("Oh, My\n", "O h M y\n") == 0);
+
+    assert(compStr("Hero\n", "Herb\n") > 0);
+
+    assert(compStr("Hello, world\n", "Hello, my world\n") > 0);
+
+    assert(compStr("My name is Anna\n", "My name is not Anna\n") < 0);
+}
+
+void sortLines(char *line[], LONG low, LONG up)//С„СѓРЅРєС†РёСЏ РїРѕР»СѓС‡Р°РµС‚ СѓРєР°Р·Р°С‚РµР»СЊ РЅР° РјР°СЃСЃРёРІ СѓРєР°Р·Р°С‚РµР»РµР№ РЅР° char
 {
     if (low >= up)
         return;
 
-    LONG mid = (low + up)/2;
-    LONG last = up;
+    LONG last = low;
 
-    swapLines(line, low, mid);//функции передаётся указатель на массив указателей char
-    mid = low;
+    swapLines(line, low, (low + up)/2);//С„СѓРЅРєС†РёРё РїРµСЂРµРґР°С‘С‚СЃСЏ СѓРєР°Р·Р°С‚РµР»СЊ РЅР° РјР°СЃСЃРёРІ СѓРєР°Р·Р°С‚РµР»РµР№ char
 
     for (int i = low + 1; i <= up; i++)
     {
-        if (compStr(line[mid], line[i]) > 0)
+        if (compStr(line[low], line[i]) > 0)
         {
-            swapLines(line, mid, i);
-            mid = i;
+            last++;
+            swapLines(line, last, i);
         }
     }
+    swapLines(line, low, last);
 
-    sortLines(line, low, mid - 1);
-    sortLines(line, mid + 1, up);
+    sortLines(line, low, last - 1);
+    sortLines(line, last + 1, up);
 }
 
-void putLine(char *line[])
+void putTheHoleLine(char *line, LONG SiZe)
 {
-    FILE *dictionary = fopen("Sorted.txt", "w");
-    fputs(*line, dictionary);
+    FILE *directory = fopen("Text.txt", "wb");
+
+    fwrite(line, sizeof(char), SiZe, directory);
+
+    fclose(directory);
+}
+
+void putLine(char *(ptr[]), char *line, LONG nlines)//РїРµСЂРІС‹Р№ РїР°СЂР°РјРµС‚СЂ - СѓРєР°Р·Р°С‚РµР»СЊ РЅР° РјР°СЃСЃРёРІ СѓРєР°Р·Р°С‚РµР»РµР№
+{
+    FILE *dictionary = fopen("Sorted.txt", "wb");
+
+    LONG SiZe = 0;
+    for (LONG i = 0; i < nlines; i++)
+    {
+        char *pt = ptr[i];
+        while(*pt != '\n')
+        {
+            fputc(*pt, dictionary);
+            printf("%c ", *pt);
+            pt++;
+        }
+        fputc(*pt++, dictionary);
+    }
+
     fclose(dictionary);
 }
-
-char getLine(char *line[], LONG num_line, int max_len, FILE *file)//функция получает указатель на массив
+void unit_tests_for_sortlines()
 {
-    LONG nsymb = 0;
-    char c = EOF;
-    while (nsymb++ < max_len && c != '\0')
     {
-        c = (char)getc(file);
-        printf("%c\n", c);
-        *line[nsymb] = c;//по адресу указателя на элемент nsymb записывается c
-        if (c == EOF)
-            return c;
+        char strinG1[4] = "ccc";
+        char strinG2[4] = "aaa";
+        char strinG3[4] = "bbb";
+        char *ptr[3] = {strinG1, strinG2, strinG3};
+        sortLines(ptr, 0, 2);
+        //printf("%c %c %c\n", *ptr[0], *ptr[1], *ptr[2]);
+        assert(ptr[0] == strinG2);
     }
-    return c;
+
+    {
+        char strinG1[6] = "ccccc";
+        char strinG2[4] = "ccc";
+        char strinG3[4] = "ccc";
+        char *ptr[3] = {strinG1, strinG2, strinG3};
+        sortLines(ptr, 0, 2);
+        //printf("%c %c %c\n", *ptr[0], *ptr[1], *ptr[2]);
+        assert((ptr[0] == strinG3 || ptr[0] == strinG2) && ptr[2] == strinG1);
+    }
+
+    {
+        char strinG1[5] = "heey";
+        char strinG2[10] = "oh my god";
+        char strinG3[11] = "oh, my god";
+        char strinG4[12] = "oh, my gosh";
+        char *ptr[4] = {strinG1, strinG2, strinG3, strinG4};
+        sortLines(ptr, 0, 3);
+        //printf("%c %c %c\n", *ptr[0], *ptr[1], *ptr[2]);
+        assert(ptr[0] == strinG1 && ptr[3] == strinG4);
+    }
 }
 
-void swapLines(char *Str[], LONG i, LONG j)//функция получает указатели на массив указателей на char
+LONG getLines(char *buf, char *ptr_mas[])
 {
-    char *temp;//указатель на char
-    strCopy(temp, Str[i]);
-    strCopy(Str[i], Str[j]);//передаются указатели на char
-    strCopy(Str[j], temp);
+    LONG nlines = 0;
+    char c = EOF;
+
+    *ptr_mas = buf;
+
+    while (nlines < MAXLINES && c != '\0')
+    {
+        c = *buf;
+        if (c == '\n')
+        {
+            nlines++;
+            ptr_mas[nlines] = ++buf;
+        }
+        else
+            buf++;
+    }
+
+    return nlines;
+}
+
+void swapLines(char *Str[], LONG i, LONG j)
+{
+    char *temp;//СѓРєР°Р·Р°С‚РµР»СЊ РЅР° char
+
+    temp = Str[i];
+    Str[i] = Str[j];
+    Str[j] = temp;
 }
